b6c54dd6cc5b8c3c5a62d5b410cf24d7fab8ac28
abul <scrucs@hotmail.com>
2015-05-25 01:23:57 -0500

    Cambios a buscador

--- buscador/js/buscar.js
+++ buscador/js/buscar.js
@@ -1,33 +1,22 @@
-var substringMatcher = function(strs) {
-  return function findMatches(q, cb) {
-    var matches, substringRegex;
- 
-    // an array that will be populated with substring matches
-    matches = [];
- 
-    // regex used to determine if a string contains the substring `q`
-    substrRegex = new RegExp(q, 'i');
- 
-    // iterate through the pool of strings and for any string that
-    // contains the substring `q`, add it to the `matches` array
-    $.each(strs, function(i, str) {
-      if (substrRegex.test(str)) {
-        matches.push(str);
-      }
-    });
- 
-    cb(matches);
-  };
-};
-
 var nombres = ['Daniel: Grupo Alfa', 'Javier: Grupo Beta', 'Pedro', 'Saul', 'Marco', 'Kathia', 'Mariel', 'Aline', 'Alberto', 'Erick', 'Ernesto'];
 
-$('#nombres .typeahead').typeahead({
+var nombres = new Bloodhound({
+  datumTokenizer: Bloodhound.tokenizers.whitespace,
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  local: nombres
+  //prefetch: 'nombres.json'
+});
+
+$('#nombre .typeahead').typeahead({
   hint: true,
   highlight: false,
   minLength: 1
 },
 {
   name: 'nombres',
-  source: substringMatcher(nombres)
+  source: nombres
+});
+
+$('.typeahead').bind('typeahead:select', function(ev, suggestion) {
+  window.alert('Selection: ' + suggestion);
 });
